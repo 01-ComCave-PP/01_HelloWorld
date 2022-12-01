1bd6933896903f67dcc6e1807ae12464f335d1b8
Sebas <cell333666@gmail.com>
2022-12-01 13:00:32 +0100

    rechner

--- script_01.js
+++ script_01.js
@@ -1,4 +1,37 @@
 
 // Hello World
 
-console.log("Hello, world!");
+let v;
+
+
+
+function plus(a, b) {
+
+    return v = a + b;
+
+}
+
+function minus(a, b) {
+
+    return v = a - b;
+
+}
+
+function rechner(a, b, c) {
+
+
+
+    if (c == "plus") {
+        v = plus(a, b);
+
+    } else if (c == "minus") {
+        v = minus(a, b);
+    }
+
+}
+
+rechner(1, 2, "plus");
+console.log("DEergebnis: " + v);
+
+rechner(2, 3, "minus");
+console.log("DEergebnis: " + v);
